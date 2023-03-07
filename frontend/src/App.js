f6ca1c5f0f130383239cb6cbfd44e3019a6ad6e6
Ryan Dalton <daltonr121@gmail.com>
2023-03-07 04:02:53 -0500

    forgot to remove junk template page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import About from "./components/About";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import List from "./components/List";
@@ -16,7 +15,6 @@ function App() {
         <Routes>
         <Route exact path="/" element={<Home />} />
           <Route exact path="/:id" element={<List />} />
-          <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
